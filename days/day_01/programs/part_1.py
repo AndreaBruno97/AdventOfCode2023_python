0345a7316278bdf86f9c8d005584787ef0fa7759
AndreaBruno97 <andbruno97@gmail.com>
2023-12-01 19:50:38 +0100

    Day 1

--- days/day_01/programs/part_1.py
+++ days/day_01/programs/part_1.py
@@ -1,4 +1,5 @@
 from common import *
+import re
 
 
 class Part_1(BaseClass):
@@ -7,11 +8,26 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        file_lines = open_file_lines(filepath)
 
-        return -1
+        # "^\D*(\d).*(\d)\D*$" matches the case with two different digits
+        # "^\D*(\d)\D*$" matches the case with a single digit
+        regex_pattern = r"^\D*(\d).*(\d)\D*$|^\D*(\d)\D*$"
+        total = 0
+
+        for cur_line in file_lines:
+            cur_match = re.search(regex_pattern, cur_line)
+            first_num = cur_match.group(1)
+            second_num = cur_match.group(2)
+            double_num = cur_match.group(3)
+
+            string_to_convert = first_num + second_num if double_num is None else double_num * 2
+
+            total += int(string_to_convert)
+
+        return total
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(142)
 p1.execute()
--- days/day_01/programs/part_2.py
+++ days/day_01/programs/part_2.py
@@ -1,4 +1,5 @@
 from common import *
+import re
 
 
 class Part_2(BaseClass):
@@ -6,12 +7,45 @@ class Part_2(BaseClass):
     def __init__(self):
         super().__init__()
 
-    def execute_internal(self, filepath):
-        print(open_file(filepath))
+    string_to_digit = {
+        "one":      "1",
+        "two":      "2",
+        "three":    "3",
+        "four":     "4",
+        "five":     "5",
+        "six":      "6",
+        "seven":    "7",
+        "eight":    "8",
+        "nine":     "9",
+    }
 
-        return -1
+    def execute_internal(self, filepath):
+        file_lines = open_file_lines(filepath)
+
+        # this regex uses "lookahead" to capture overlapping strings
+        # example: "8oneightgp" should return "8" and "eight", but without lookahead
+        # it returns "8" and "one", because when the regex matches "one", it
+        # consumes the characters, and the remaining string is only "ightgp", which doesn't match anything.
+        # Lookahead allows you to find all starting positions for matching strings, without consuming them.
+        regex_pattern = r"(?=(\d|" + "|".join(self.string_to_digit.keys()) + "))"
+        total = 0
+
+        for cur_line in file_lines:
+            cur_match_list = re.findall(regex_pattern, cur_line)
+            first_num = cur_match_list[0]
+            last_num = cur_match_list[-1]
+
+            if first_num in self.string_to_digit:
+                first_num = self.string_to_digit[first_num]
+
+            if last_num in self.string_to_digit:
+                last_num = self.string_to_digit[last_num]
+
+            total += int(first_num + last_num)
+
+        return total
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(142, [("example_2", 281)])
 p2.execute()
