31daaaabbabf16e5f72600ca663c5a834fa22c7c
AndreaBruno97 <andbruno97@gmail.com>
2023-12-02 14:13:49 +0100

    Day 2

--- days/day_02/programs/part_1.py
+++ days/day_02/programs/part_1.py
@@ -7,11 +7,39 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        file_lines = open_file_lines(filepath)
 
-        return -1
+        max_cubes = {}
+        # The first line of the input contains the list of supposed amount of cubes per color
+        for cur_cube in file_lines[0].split(", "):
+            cur_max_num, cur_color = cur_cube.split(" ")
+            max_cubes[cur_color] = int(cur_max_num)
+
+        id_sum = 0
+
+        for cur_game in file_lines[1:]:
+            game_str, round_list_str = cur_game.split(": ")
+            cur_game = int(game_str.replace("Game ", ""))
+
+            found = False
+            for cur_round in round_list_str.split("; "):
+
+                for cur_cube in cur_round.split(", "):
+                    cur_cube_num, cur_cube_color = cur_cube.split(" ")
+
+                    if int(cur_cube_num) > max_cubes[cur_cube_color]:
+                        found = True
+                        break
+
+                if found is True:
+                    break
+
+            if found is False:
+                id_sum += cur_game
+
+        return id_sum
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(8)
 p1.execute()
--- days/day_02/programs/part_2.py
+++ days/day_02/programs/part_2.py
@@ -1,4 +1,5 @@
 from common import *
+import functools as ft
 
 
 class Part_2(BaseClass):
@@ -7,11 +8,27 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        file_lines = open_file_lines(filepath)
 
-        return -1
+        power_sum = 0
+
+        for cur_game in file_lines[1:]:
+            min_cubes = {}
+            _, round_list_str = cur_game.split(": ")
+
+            for cur_round in round_list_str.split("; "):
+                for cur_cube in cur_round.split(", "):
+                    cur_cube_num, cur_cube_color = cur_cube.split(" ")
+                    cur_cube_num = int(cur_cube_num)
+
+                    if cur_cube_color not in min_cubes or cur_cube_num > min_cubes[cur_cube_color]:
+                        min_cubes[cur_cube_color] = cur_cube_num
+
+            power_sum += ft.reduce(lambda a, b: a*b, min_cubes.values())
+
+        return power_sum
 
 
-p2 = Part_2()
-p2.test(0)
-p2.execute()
+p1 = Part_2()
+p1.test(2286)
+p1.execute()
