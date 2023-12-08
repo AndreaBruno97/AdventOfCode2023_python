52c81053f8c6a5a265f4bbcabdbc646e3eab6479
AndreaBruno97 <andbruno97@gmail.com>
2023-12-08 15:59:52 +0100

    Day 8

--- days/day_08/programs/part_1.py
+++ days/day_08/programs/part_1.py
@@ -7,11 +7,34 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        input_list = open_file_lines(filepath)
 
-        return -1
+        instruction_list = list(input_list[0])
+
+        graph = {}
+        for cur_line in input_list[2:]:
+            start_node, end_couple = cur_line.split(" = ")
+            end_node_left, end_node_right = end_couple.replace("(", "").replace(")", "").split(", ")
+
+            graph[start_node] = (end_node_left, end_node_right)
+
+        modulus = len(instruction_list)
+        steps_num = 0
+        cur_node = "AAA"
+
+        while cur_node != "ZZZ":
+            cur_turn = instruction_list[steps_num % modulus]
+
+            if cur_turn == "L":
+                cur_node = graph[cur_node][0]
+            else:
+                cur_node = graph[cur_node][1]
+
+            steps_num += 1
+
+        return steps_num
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(2, [("example_2.txt", 6)])
 p1.execute()
--- days/day_08/programs/part_2.py
+++ days/day_08/programs/part_2.py
@@ -1,4 +1,5 @@
 from common import *
+import math
 
 
 class Part_2(BaseClass):
@@ -7,11 +8,37 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        input_list = open_file_lines(filepath)
 
-        return -1
+        instruction_list = list(input_list[0])
+
+        graph = {}
+        for cur_line in input_list[2:]:
+            start_node, end_couple = cur_line.split(" = ")
+            end_node_left, end_node_right = end_couple.replace("(", "").replace(")", "").split(", ")
+
+            graph[start_node] = (end_node_left, end_node_right)
+
+        modulus = len(instruction_list)
+        cur_node_set = set([x for x in graph.keys() if x[2] == "A"])
+        path_length_list = []
+
+        for cur_node in cur_node_set:
+            steps_num = 0
+            while cur_node[2] != "Z":
+                cur_turn = instruction_list[steps_num % modulus]
+
+                if cur_turn == "L":
+                    cur_node = graph[cur_node][0]
+                else:
+                    cur_node = graph[cur_node][1]
+
+                steps_num += 1
+            path_length_list.append(steps_num)
+
+        return math.lcm(*path_length_list)
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(2, [("example_2.txt", 6), ("example_3.txt", 6)])
 p2.execute()
