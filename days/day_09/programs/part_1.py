ee0349e9d75ca46beeedb9df4818c22bb0f77134
AndreaBruno97 <andbruno97@gmail.com>
2023-12-09 15:09:39 +0100

    Day 9

--- days/day_09/programs/part_1.py
+++ days/day_09/programs/part_1.py
@@ -1,4 +1,5 @@
 from common import *
+import numpy as np
 
 
 class Part_1(BaseClass):
@@ -6,12 +7,24 @@ class Part_1(BaseClass):
     def __init__(self):
         super().__init__()
 
-    def execute_internal(self, filepath):
-        print(open_file(filepath))
+    def next_number(self, cur_list):
+        if np.all(cur_list == 0):
+            return 0
 
-        return -1
+        cur_last_num = cur_list[-1]
+        next_num = self.next_number(np.diff(cur_list))
+        return cur_last_num + next_num
+
+    def execute_internal(self, filepath):
+        history_list = [np.array([int(y) for y in x.split(" ")]) for x in open_file_lines(filepath)]
+
+        total = 0
+        for cur_history in history_list:
+            total += self.next_number(cur_history)
+
+        return total
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(114)
 p1.execute()
--- days/day_09/programs/part_2.py
+++ days/day_09/programs/part_2.py
@@ -1,4 +1,5 @@
 from common import *
+import numpy as np
 
 
 class Part_2(BaseClass):
@@ -6,12 +7,24 @@ class Part_2(BaseClass):
     def __init__(self):
         super().__init__()
 
-    def execute_internal(self, filepath):
-        print(open_file(filepath))
+    def first_number(self, cur_list):
+        if np.all(cur_list == 0):
+            return 0
 
-        return -1
+        cur_first_num = cur_list[0]
+        prev_num = self.first_number(np.diff(cur_list))
+        return cur_first_num - prev_num
+
+    def execute_internal(self, filepath):
+        history_list = [np.array([int(y) for y in x.split(" ")]) for x in open_file_lines(filepath)]
+
+        total = 0
+        for cur_history in history_list:
+            total += self.first_number(cur_history)
+
+        return total
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(2)
 p2.execute()
