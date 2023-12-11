2b4775a426dba87b0740555f62cebe12adef428d
AndreaBruno97 <andbruno97@gmail.com>
2023-12-11 19:29:49 +0100

    Day 11

--- days/day_11/programs/part_1.py
+++ days/day_11/programs/part_1.py
@@ -1,4 +1,6 @@
 from common import *
+import numpy as np
+import itertools
 
 
 class Part_1(BaseClass):
@@ -7,11 +9,26 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        matrix = np.array(open_file_str_matrix(filepath))
 
-        return -1
+        # Expand rows
+        matrix = np.repeat(matrix, [2 if np.all(row == '.') else 1 for row in matrix], axis=0)
+        # Expand columns
+        matrix = np.repeat(matrix, [2 if np.all(col == '.') else 1 for col in matrix.T], axis=1)
+
+        galaxy_list = [(x[0], x[1]) for x in np.transpose(np.where(matrix == '#'))]
+        galaxy_couples = set(itertools.combinations(galaxy_list, 2))
+
+        total = 0
+        for start_galaxy, end_galaxy in galaxy_couples:
+            start_galaxy_x, start_galaxy_y = start_galaxy
+            end_galaxy_x, end_galaxy_y = end_galaxy
+            # Manhattan distance
+            total += abs(end_galaxy_x - start_galaxy_x) + abs(end_galaxy_y - start_galaxy_y)
+
+        return total
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(374)
 p1.execute()
--- days/day_11/programs/part_2.py
+++ days/day_11/programs/part_2.py
@@ -1,4 +1,6 @@
 from common import *
+import numpy as np
+import itertools
 
 
 class Part_2(BaseClass):
@@ -7,11 +9,33 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        matrix = np.array(open_file_str_matrix(filepath))
 
-        return -1
+        # List of empty rows
+        empty_rows_list = np.where([np.all(row == '.')for row in matrix])[0]
+        # List of empty columns
+        empty_cols_list = np.where([np.all(col == '.') for col in matrix.T])[0]
+
+        # Add (1000000 - 1) to each coordinate value for each empty row/column before it
+        # Each row/column is already counted once in the actual coordinate value of the galaxy
+        expansion_factor = 1000000 if "example"not in filepath else 100
+        galaxy_list = [(
+            x[0] + ((expansion_factor - 1) * len(np.where(empty_rows_list < x[0])[0])),
+            x[1] + ((expansion_factor - 1) * len(np.where(empty_cols_list < x[1])[0])))
+            for x in np.transpose(np.where(matrix == '#'))]
+
+        galaxy_couples = set(itertools.combinations(galaxy_list, 2))
+
+        total = 0
+        for start_galaxy, end_galaxy in galaxy_couples:
+            start_galaxy_x, start_galaxy_y = start_galaxy
+            end_galaxy_x, end_galaxy_y = end_galaxy
+            # Manhattan distance
+            total += abs(end_galaxy_x - start_galaxy_x) + abs(end_galaxy_y - start_galaxy_y)
+
+        return total
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(8410)   # Example with expansion factor of 100
 p2.execute()
